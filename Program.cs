8dfd6f0556e776338c6665513bb0ffd8af2c0dbe
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-05-28 10:38:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your forename:");
+            string forename = Console.ReadLine();
+            Console.WriteLine("Please enter your surname:");
+            string surname = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"It is nice to meet you, {forename} {surname}!");
+
+            Console.WriteLine();
+            Console.WriteLine("May we have your date of birth, please? Supply it as YYYY/MM/DD.");
+            Console.WriteLine("Year:");
+            int yearBorn = int.Parse(Console.ReadLine());
+            Console.WriteLine("Month:");
+            int monthBorn = int.Parse(Console.ReadLine());
+            Console.WriteLine("Day:");
+            int dayBorn = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine();
+            DateTime dateOfBirth = new DateTime(yearBorn, monthBorn, dayBorn);
+            DateTime today = DateTime.Today;
+            int age = Convert.ToInt32(today.Year) - Convert.ToInt32(dateOfBirth.Year);
+           
+
+            Console.WriteLine($"Excellent, it follows that you are currently {age} years old.");
+        }
+
+    }
+}
